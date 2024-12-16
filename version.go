a8814be597626a57b79c61ff3dc66d370d7249d7
David Hu <coolbor@gmail.com>
2024-12-16 16:38:23 +0800

    chore: bump version to 1.0.4

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package jsonpath
 
 // Version is the current version of jsonpath
-const Version = "1.0.3"
+const Version = "1.0.4"
 
 // VersionWithPrefix returns the version with v prefix
 func VersionWithPrefix() string {
